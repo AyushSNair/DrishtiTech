f6fdb0a86dcbdf66993a22d1e293a2a0682d6ec3
MelissaSequeira <melissasequeira572@gmail.com>
2025-04-14 15:43:36 +0530

    responsive1

--- Frontend/drishti-site/src/pages/Auth.jsx
+++ Frontend/drishti-site/src/pages/Auth.jsx
@@ -37,7 +37,9 @@ const Auth = () => {
         ...(isSignUp && { name: formData.name }),
       };
 
-      const response = await fetch(`http://localhost:5000/api/auth/${endpoint}`, {
+
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/${endpoint}`, {
+
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(bodyData),
